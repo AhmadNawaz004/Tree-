a3098514fec425ecf663598049735fc7936fccef
Muhammad Ahmad Nawaz <75672446+AhmadNawaz004@users.noreply.github.com>
2021-01-14 16:32:12 +0500

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,27 @@
+
+
+public class Tree {
+
+    private TreeNode root;
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+        }
+        else {
+            root.insert(value);
+        }
+    }
+
+    public void traverseInOrder() {
+        if (root != null) {
+            root.traverseInOrder();
+        }
+    }
+    public void  traversePreOrder() {
+        if (root != null) {
+            root. traversePreOrder();
+        }
+    }
+
+}
